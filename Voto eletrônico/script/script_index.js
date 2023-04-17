9c8325745c079115d2706b3df3e69b4d38b3f6e0
Ricardo Santos <105801932+ricardosantos3012@users.noreply.github.com>
2023-04-17 19:11:44 -0300

    projeto voto eletronico

---
+++ Voto eletrônico/script/script_index.js
@@ -0,0 +1,45 @@
+const candidates = [];
+const candidateName = document.querySelector('.candidate-name');
+const candidateNumber = document.querySelector('.candidate-number');
+
+const clearCamp = () => {
+    candidateName.value = '';
+    candidateNumber.value = '';
+    candidateName.focus();
+}
+
+const display = (name, number) => {
+    const list = document.querySelector('.list');
+    const listCand = document.createElement('p');
+
+    list.appendChild(listCand);
+    listCand.innerText += (`Nome: ${name} - Número: ${number}`);
+}
+
+const register =  () => {
+    document.addEventListener('click', (event) => {
+        const element = event.target;
+    
+        if (element.classList.contains('register')) {
+            if(candidateName.value === '' || candidateNumber.value === '') {
+                alert('Insira todos os dados para prosseguir com o cadastro');
+                clearCamp();
+                return;
+            }
+            if(candidateNumber.value == NaN) {
+                alert('Você deve inserir apenas números');
+                return;
+            }
+            candidates.push({name: candidateName.value, number: candidateNumber.value});
+            console.log(candidates);
+            alert('Candidato cadastrado com sucesso');
+            display(candidateName.value, candidateNumber.value);
+            clearCamp();
+        }
+        if(element.classList.contains('finalize-registration')) {
+            alert('Você será direcionado para a tela de votação');
+        }
+    });
+}
+
+register();
