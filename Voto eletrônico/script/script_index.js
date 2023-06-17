567674bdda805735c5bd0b079746bb982b69ec85
Ricardo Santos <105801932+ricardosantos3012@users.noreply.github.com>
2023-06-16 21:07:29 -0300

    projeto individual

--- Voto eletrônico/script/script_index.js
+++ Voto eletrônico/script/script_index.js
@@ -64,78 +64,120 @@ class UrnaEletronica {
         this.ul2.appendChild(p);
         p.innerText = (`Nome: ${name} - Número: ${number}`);
     }
+
+    insert() { //Verificar método de comparação de dados(nomes e números iguais)
+        if(this.candidateName.value === '' || this.candidateNumber.value === '') {
+            this.message('Preencha os campos corretamente para efetuar o cadastro');
+            this.alertMessage();
+            return;
+        }
+        if(isNaN(this.candidateNumber.value)) {
+            this.message('Você deve inserir apenas números');
+            this.candidateNumber.value = '';
+            this.alertMessage();
+            return;
+        }
+        for(let value of this.registeredCandidates) {
+            if(value.name === this.candidateName.value || value.number === this.candidateNumber.value) {
+                this.message(`Você não pode registrar dois candidatos com o mesmo nome ou número.`);
+                this.alertMessage();
+                this.clearCamp();
+            }
+            return;
+        }
+        this.registeredCandidates.push(this.candidates(this.candidateName.value, this.candidateNumber.value));
+        console.log(this.registeredCandidates);
+        this.generateList(this.candidateName.value, this.candidateNumber.value);
+        this.generateListTwo(this.candidateName.value, this.candidateNumber.value);
+        this.clearCamp();
+        this.alertMessage();
+    }
+
+    finalize() {
+        if(this.registeredCandidates.length < 1) {
+            this.message('Não há candidatos cadastrados para efetuar a votação.');
+            this.alertMessage();
+            return;
+        }
+        if(this.registeredCandidates.length < 2) {
+            this.message('Você não pode realizar uma votação com apenas um candidato.');
+            this.alertMessage();
+            return;
+        }
+        alert('Você será direcionado para a tela de votação');
+        this.container.style.display = 'none';
+        this.votePage.style.display = 'block';
+    }
+
+    confirm() {
+        if(this.insertVote.value === '') {
+            this.whiteVote++;
+            alert(`Voto branco registrado.`);
+        } else {
+            for(let value of this.registeredCandidates) {
+                if(value.number === this.insertVote.value) {
+                    value.vote++;
+                    alert(`Voto válido registrado`);
+                    console.log(value);
+                    this.insertVote.value = '';
+                }
+            }
+            for(let value of this.registeredCandidates) {
+                if(this.insertVote.value === '') {
+                    return;
+                }
+                break;
+            }
+            for(let value of this.registeredCandidates) {
+                if(value.number !== this.insertVote.value) {
+                    this.nullVote++;
+                    alert(`Voto nulo registrado.`);
+                    this.insertVote.value = '';
+                    console.log(this.nullVote);
+                }
+                break;
+            }
+        }
+    }
+
+    back() {
+        alert('Você será direcionado para a tela de cadastro e todos os dados serão perdidos.');
+        this.whiteVote = 0;
+        this.nullVote = 0;
+        for(let value of this.registeredCandidates) {
+            value.vote = 0;
+            console.log(value.vote);
+        }
+        this.container.style.display = 'block';
+        this.votePage.style.display = 'none';
+    }
+
+    finalizeVote() {
+
+    }
     
     register() {
         document.addEventListener('click', (event) => {
             const element = event.target;
     
             if(element.classList.contains('register')) {
-                if(this.candidateName.value === '' || this.candidateNumber.value === '') {
-                    this.message('Preencha os campos corretamente para efetuar o cadastro');
-                    this.alertMessage();
-                    return;
-                }
-                if(isNaN(this.candidateNumber.value)) {
-                    this.message('Você deve inserir apenas números');
-                    this.candidateNumber.value = '';
-                    this.alertMessage();
-                    return;
-                }
-                this.registeredCandidates.push(this.candidates(this.candidateName.value, this.candidateNumber.value));
-                console.log(this.registeredCandidates);
-                this.generateList(this.candidateName.value, this.candidateNumber.value);
-                this.generateListTwo(this.candidateName.value, this.candidateNumber.value);
-                this.clearCamp();
-                this.alertMessage();
+                this.insert();
             }
             if(element.classList.contains('finalize')) {
-                if(this.registeredCandidates.length < 1) {
-                    this.message('Não há candidatos cadastrados para efetuar a votação.');
-                    this.alertMessage();
-                    return;
-                }
-                if(this.registeredCandidates.length < 2) {
-                    this.message('Você não pode realizar uma votação com apenas um candidato.');
-                    this.alertMessage();
-                    return;
-                }
-                alert('Você será direcionado para a tela de votação');
-                this.container.style.display = 'none';
-                this.votePage.style.display = 'block';
+                this.finalize();
             }
             if(element.classList.contains('to-go-back')) {
-                alert('Você será direcionado para a tela de cadastro e todos os dados serão perdidos.');
-    
+                this.back();
             }
             if(element.classList.contains('confirm')) {
-                if(this.insertVote.value === '') {
-                    this.whiteVote++;
-                    alert(`Voto branco registrado.`);
-                } else {
-                    for(let value of this.registeredCandidates) {
-                        if(value.number === this.insertVote.value) {
-                            value.vote++;
-                            alert(`Voto válido registrado`);
-                            console.log(value);
-                            this.insertVote.value = '';
-                        }
-                    }
-                    for(let value of this.registeredCandidates) {
-                        if(this.insertVote.value === '') {
-                            return;
-                        }
-                        break;
-                    }
-                    for(let value of this.registeredCandidates) {
-                        if(value.number !== this.insertVote.value) {
-                            this.nullVote++;
-                            alert(`Voto nulo registrado.`);
-                            this.insertVote.value = '';
-                            console.log(this.nullVote);
-                        }
-                        break;
-                    }
-                }
+                this.confirm();
+            }
+            if(element.classList.contains('white')) {
+                this.whiteVote++;
+                alert(`Voto Branco registrado.`);
+            }
+            if(element.classList.contains('finalize-vote')) {
+                this.finalizeVote();
             }
         });
     }
